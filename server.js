d4a008bc03058805a9ad32980f6214d8d1bca5c3
Armando Alamilla Cuellar <armando_alamilla10@hotmail.com>
2021-03-26 06:24:53 -0600

    Body and query info

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
 const port = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+
 app.get('/message', (req, res) => {
+  console.log(req.query)
   res.send('Message list')
 })
 
 app.post('/message', (req, res) => {
+  console.log(req.body)
   res.send('Message added')
 })
 
