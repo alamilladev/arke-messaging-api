88a6744da78ee700b5e5c8a39a60ed84a2e39084
Armando Alamilla Cuellar <armando_alamilla10@hotmail.com>
2021-03-27 18:26:56 -0600

    static file added to serve

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// import required modules
 const express = require('express')
 const bodyParser = require('body-parser')
 const response = require('./network/response')
@@ -5,9 +6,11 @@ const response = require('./network/response')
 const app = express()
 const port = process.env.PORT || 3000
 
+// Configure bodyParser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// List messages
 app.get('/message', (req, res) => {
   console.log(req.headers)
   console.log(req.query)
@@ -19,6 +22,7 @@ app.get('/message', (req, res) => {
   response.success(req, res, 200, 'Mesage list')
 })
 
+// create new message
 app.post('/message', (req, res) => {
   if (req.query.error === 'true') {
     response.error(req, res, 400, 'Simulated error')
@@ -26,6 +30,9 @@ app.post('/message', (req, res) => {
     response.success(req, res, 201, 'Message created successfully')
   }
 })
+
+// serve a static file
+app.use('/app', express.static('public'))
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
